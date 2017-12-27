c4ab58ff613988bfeee0a7e7dfdc9244e33e878f
chicagosoles <chicaogosoles@users.noreply.github.com>
2017-12-26 21:38:07 -0600

    Organization

--- Monitor 1.2.py
+++ Monitor 1.2.py
@@ -10,6 +10,8 @@ newlist = []
 response = requests.get(sitemap, headers=scrapeHeaders)
 soup = BeautifulSoup(response.text, "html.parser")
 initialcount = 0
+
+#Sets the initial amount of product links and sends startup info to Discord 
 def startup():
     global initialcount
     initialcount = getproducts()
@@ -25,12 +27,14 @@ def startup():
         ]
     }
     r = requests.post('https://discordapp.com/api/webhooks/394374752975978496/SG6WZHXlHcTLJd8er0vlrzr5dNGvNVHCXdZeumQozItpIi821Dls3A2i9iOL-p0Pg8-I', headers=discordHeaders, json=body)
-    return initialcount
+
+
 def reset():
     oglist = []
     newlist = []
     return oglist, newlist
 
+#Returns the number of product links in the sitemap
 def getproducts():
     counter = 0
     for loc in soup.find_all("loc"):
@@ -38,19 +42,25 @@ def getproducts():
     return counter
 
 def monitor():
-    print (str(initialcount))
+    #Compiles a list of all product links in the sitemap
     for loc in soup.find_all("loc"):
         link = loc.text
         oglist.append(link)
+        
     while True:
+        #Waits for a random time
         waittime = random.randrange(5,10,1)
         print ("Sleeping for " + str(waittime) + " seconds...")
         time.sleep(waittime)
+        
+        #For the amount of times loc appears in the sitemap
         for loc in soup.find_all("loc"):
             comparelink = loc.text
+            
+            #If the link is not in the original list of links
             if not comparelink in oglist:
                 print ("New product has been detected!")
-                ##Sends new link and the number of products loaded to the Discord webhook
+                #Sends new link and the number of products loaded to the Discord webhook
                 body = {
                     "content": "",
                     "embeds": [
@@ -62,27 +72,25 @@ def monitor():
                               ]
                         }
                 r = requests.post('https://discordapp.com/api/webhooks/394374752975978496/SG6WZHXlHcTLJd8er0vlrzr5dNGvNVHCXdZeumQozItpIi821Dls3A2i9iOL-p0Pg8-I', headers=discordHeaders, json=body)
-            comparecount = getproducts()
-            if comparecount > initialcount:
-                body = {
-                    "content": "",
-                    "embeds": [
-                        {
-                            "title": "New Product But monitor didnt get the link",
-                            "description":  ("Number of Products loaded now vs start :" + str(initialcount) + " vs " + str(comparecount))
-                        }
-                                ]
-                        }
-                r = requests.post('https://discordapp.com/api/webhooks/394374752975978496/SG6WZHXlHcTLJd8er0vlrzr5dNGvNVHCXdZeumQozItpIi821Dls3A2i9iOL-p0Pg8-I', headers=discordHeaders, json=body)
-      
-            
-        
-    
-        
-    return oglist
 
+        #This is a test to see if the number of links increased but the comparelink method did not pick it up
+        comparecount = getproducts()
+        if comparecount > initialcount:
+            body = {
+                "content": "",
+                "embeds": [
+                    {
+                        "title": "New Product But monitor didnt get the link",
+                        "description":  ("Number of Products loaded now vs start :" + str(initialcount) + " vs " + str(comparecount))
+                    }
+                            ]
+                    }
+            r = requests.post('https://discordapp.com/api/webhooks/394374752975978496/SG6WZHXlHcTLJd8er0vlrzr5dNGvNVHCXdZeumQozItpIi821Dls3A2i9iOL-p0Pg8-I', headers=discordHeaders, json=body)
+    return oglist
+            
 def getoglist():
     return oglist
+
 
 startup()
 monitor()
